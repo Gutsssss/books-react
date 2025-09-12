212e3c873d66fe1d6a6e60dac1ea9cb757cbf543
Gutsssss <kacill1@mail.ru>
2025-09-12 15:46:16 +0500

    add debounce hook for serach books api

--- src/pages/books/index.tsx
+++ src/pages/books/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../shared/hooks/redux";
 import { closeDialog, fetchBooks, loadMore } from "../../entities/books/model/BookSlice";
 import {
@@ -13,21 +13,34 @@ import {
   Typography,
 } from "@mui/material";
 import { BooksTable } from "../../entities/books/ui/BooksTable/BooksTable";
+import { useDebounce } from "../../shared/hooks/useDebounce";
 export const BooksPage = () => {
   const dispatch = useAppDispatch();
-  const { books, isLoading, moreBooksLoading,currentBook,isDialogOpen } = useAppSelector(
+  const load = useRef(null)
+  const { books, isLoading, moreBooksLoading, currentBook, isDialogOpen } = useAppSelector(
     (state) => state.bookReducer
   );
-  const [searchParam, setSearchParam] = useState("book");
+  const [searchParam, setSearchParam] = useState("");
+  const debounceSearch = useDebounce(searchParam, 500)
+
   useEffect(() => {
-    dispatch(fetchBooks("books"));
-  }, [dispatch]);
-  useEffect(() => {
-    dispatch(fetchBooks(searchParam));
-  }, [dispatch, searchParam]);
+    if (!debounceSearch.trim()) {
+      dispatch(fetchBooks("books"));
+    }
+    else {
+      dispatch(fetchBooks(debounceSearch))
+    }
+    console.log(load.current)
+  }, [dispatch, debounceSearch,load]);
   const loadMoreBooks = () => {
     try {
-      dispatch(loadMore(searchParam));
+      if (!debounceSearch.trim()) {
+        dispatch(loadMore("books"));
+      }
+      else {
+        dispatch(loadMore(debounceSearch));
+      }
+
     } catch (error) {
       console.log(error);
     }
@@ -46,36 +59,36 @@ export const BooksPage = () => {
           label="Поиск"
           sx={{ marginY: "10px" }}
         />
-        <BooksTable books={books} loading={isLoading}/>
+        <BooksTable books={books} loading={isLoading} />
         {currentBook && (
-        <Dialog
-          open={isDialogOpen}
-          onClose={handleCloseDialog}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            {currentBook.volumeInfo.title}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              <span style={{fontWeight:"bold"}}>Описание:</span> {currentBook.volumeInfo.description || 'Описание отсутствует'}
-            </DialogContentText>
-            <DialogContentText id="alert-dialog-publishedDate">
-              <span style={{fontWeight:"bold"}}>Дата публикации:</span> {currentBook.volumeInfo.publishedDate || 'Дата публикации отсутствует'}
-            </DialogContentText>
-            {currentBook?.volumeInfo.authors ? <DialogContentText id="alert-dialog-authors">
-              <span style={{fontWeight:"bold"}}>Авторы:</span> {currentBook?.volumeInfo.authors?.map((author,index) => {
-                return <Typography key={`${author}${index}`}>{author}</Typography>
-              })}
-            </DialogContentText> : <DialogContentText>Авторы отсутствуют</DialogContentText>}
-            
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseDialog}>Закрыть</Button>
-          </DialogActions>
-        </Dialog>
-      )}
+          <Dialog
+            open={isDialogOpen}
+            onClose={handleCloseDialog}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {currentBook.volumeInfo.title}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                <span style={{ fontWeight: "bold" }}>Описание:</span> {currentBook.volumeInfo.description || 'Описание отсутствует'}
+              </DialogContentText>
+              <DialogContentText id="alert-dialog-publishedDate">
+                <span style={{ fontWeight: "bold" }}>Дата публикации:</span> {currentBook.volumeInfo.publishedDate || 'Дата публикации отсутствует'}
+              </DialogContentText>
+              {currentBook?.volumeInfo.authors ? <DialogContentText id="alert-dialog-authors">
+                <span style={{ fontWeight: "bold" }}>Авторы:</span> {currentBook?.volumeInfo.authors?.map((author, index) => {
+                  return <Typography key={`${author}${index}`}>{author}</Typography>
+                })}
+              </DialogContentText> : <DialogContentText>Авторы отсутствуют</DialogContentText>}
+
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCloseDialog}>Закрыть</Button>
+            </DialogActions>
+          </Dialog>
+        )}
         {!isLoading && (
           <Button
             sx={{ marginY: "10px" }}
@@ -87,8 +100,8 @@ export const BooksPage = () => {
             Загрузить больше
           </Button>
         )}
-        {}
       </Box>
+      <span ref={load} style={{background:"red"}}>qwe</span>
     </>
   );
 };
